26d7b53f2d0e80df089785d5c9a2d2c622bbb1fe
mahima <076bct033.mahima@pcampus.edu.np>
2022-01-21 15:39:30 +0545

    latest

--- news/models.py
+++ news/models.py
@@ -4,9 +4,18 @@ from django.db import models
 class files(models.Model):
     title = models.CharField(max_length=200)
     file = models.BinaryField()
+    institution=models.CharField(max_length=200)
+    url=models.CharField(max_length=200)
+    
     #check??
     #make folder attribute by yourself
+    language=models.CharField(max_length=100)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    creation_date = models.CharField(max_length=200)
+    preview = models.BinaryField()
+
+   
+
 
 #parameters_add
 # institution(url parse)
--- news/views.py
+++ news/views.py
@@ -6,12 +6,84 @@ from bs4 import BeautifulSoup
 from news.models import files
 from django.core.files import File
 from urllib.parse import urlparse
+import fitz
+from PyPDF2 import PdfFileReader, PdfFileWriter
 
-urls = ['https://nepalindata.com/resource/category/laws-policies-and-strategies/', 'http://www.gatsby.ucl.ac.uk/teaching/courses/ml1-2016.html', 'https://nepalconsular.gov.np/np/',
-        'http://www.dotm.gov.np/', 'https://mofa.gov.np/', 'https://merolagani.com/', 'https://www.dop.gov.np/', 'http://ciaa.gov.np/', 'https://mohp.gov.np/']
+
+
+def language(filepath):
+    from langdetect import detect
+    from pdfminer3.layout import LAParams
+    from pdfminer3.pdfpage import PDFPage
+    from pdfminer3.pdfinterp import PDFResourceManager
+    from pdfminer3.pdfinterp import PDFPageInterpreter
+    from pdfminer3.converter import TextConverter
+    import io
+    i=0
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+
+    with open(filepath, 'rb') as fh:
+
+        for page in PDFPage.get_pages(fh,
+                                    caching=True,
+                                    check_extractable=True):
+            page_interpreter.process_page(page)
+
+        texts = fake_file_handle.getvalue()
+    
+
+    # close open handles
+    converter.close()
+    fake_file_handle.close()
+
+    # print(text)
+    special_characters = "!@#$%^&*()-+?_=,<>/[]{}|\~`"
+    # texts="My name %5 मेरो नाम महिमा ढकाल हो |"
+    j=0
+    n_count=0
+    e_count=0
+   
+    text=texts.split(' ')
+    # print(text)
+    for txt in text:
+        txt=text[j]
+        if any(c in special_characters for c in txt) or  txt.isnumeric()==True :       
+            # print(txt) 
+            
+            j=j+1     
+            
+            continue
+        else:
+            if(len(txt)>=5 and len(txt)<=10):
+                # print(txt)
+                if (detect(txt)=='ne' or detect(txt)=='hi'):
+                    n_count=n_count+1
+                else:
+                    e_count=e_count+1   
+        j=j+1  
+    # print("Nepali:", n_count)
+    # print("English:" ,e_count) 
+    if(n_count>e_count):
+       return "Nepali"
+    else:
+       return "English" 
+
+
+
+list=[]
+
+urls = ['http://mohp.gov.np/', 'http://nepalindata.com/resource/category/laws-policies-and-strategies/', 'http://nepalconsular.gov.np/np/',
+        'http://www.dotm.gov.np/', 'http://mofa.gov.np/', 'http://merolagani.com/', 'http://www.dop.gov.np/', 'http://ciaa.gov.np/','http://www.gatsby.ucl.ac.uk/teaching/courses/ml1-2016.html']
 i=0
+
+# urls=['http://www.gatsby.ucl.ac.uk/teaching/courses/ml1-2016.html']
+
 for url in urls:
-    i=i+1
+    print(url)
+
     drive='D:\\'
     folder='data'
  
@@ -21,33 +93,62 @@ for url in urls:
         directory = folders.split('.')[1]
     else:
         directory=folders.split('.')[0]
-    # folder_location = r'D:\nid\web'+str(i)
+
+
     folder_location = os.path.join(drive,folder,directory)
-    print(folder_location)
+
+    # print(folder_location)
     if not os.path.exists(folder_location):os.mkdir(folder_location)
 
     response = requests.get(url)
     soup= BeautifulSoup(response.text, "html.parser")     
 
     for link in soup.select("a[href$='.pdf']"):
+    
         #sees anchor tag , check hrefs,$ means end of the string and at end searches .pdf extension
         #Name the pdf files using the last portion of each link which are unique in this case
-        filename = os.path.join(folder_location,link['href'].split('/')[-1]) 
-        #returns the last name by splitting all the paths separated by /
+        filename = os.path.join(folder_location,link['href'].split('/')[-1])
+        print(filename)
+        # print(i)
+        # for file in list:
+        #     if(file==filename):
+        #         break
+        #     else:     
+        #         list=list+[filename]
         with open(filename, 'wb') as f:
             
             file_in_db=os.path.join(directory,filename)
             b_name=os.path.basename(filename)
             file_in_db=os.path.join(directory,b_name)
+            
+           
             f.write(requests.get(urljoin(url,link['href'])).content)
             f.close()
-            # path='D:\\nid\web'+str(i)+'\\'+filename
-            filepath=os.path.join(drive,folder,directory,b_name)
-            file2=open(filepath, 'rb')
-            file2=files(title=file_in_db)
+            
 
-            file2.save()
+            #changes IN TITLE
+            filepath=os.path.join(drive,folder,directory,b_name)
+            # i=i+1
+            pdf=fitz.open(filepath)
+            meta=pdf.metadata          
           
 
+            #CHANGES IN TIME
+            date=meta['creationDate']
+            
+            if len(date)<14:
+               
+                f_date=date
+            else:
+                date=meta['creationDate'].split(':')[1]
+                f_date=date[0:4]+"-"+date[4:6]+"-"+date[6:8]+" "+date[8:10]+":"+date[10:12]+":"+date[12:14]
+            print("date",f_date)
+
+            language_OF_file=language(filepath)
+            # language_OF_file='NOt recognized'
+            file2=files(title=file_in_db,creation_date=f_date,institution=directory,url=url,language=language_OF_file)        
+          
+            file2.save()
+            
 
 

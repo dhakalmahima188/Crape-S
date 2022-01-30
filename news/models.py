eeafbbf5d15a1230c644e0f7a1b24c54e564c330
mahima <076bct033.mahima@pcampus.edu.np>
2022-01-30 22:31:32 +0545

    title scraped and database maintained

--- news/models.py
+++ news/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class files(models.Model):
     title = models.CharField(max_length=200)
+    fullfile = models.CharField()
     file = models.BinaryField()
     institution=models.CharField(max_length=200)
     url=models.CharField(max_length=200)
--- news/views.py
+++ news/views.py
@@ -7,7 +7,10 @@ from news.models import files
 from django.core.files import File
 from urllib.parse import urlparse
 import fitz
-from PyPDF2 import PdfFileReader, PdfFileWriter
+
+from langdetect import detect
+
+
 
 
 
@@ -16,14 +19,59 @@ from PyPDF2 import PdfFileReader, PdfFileWriter
 list=[]
 
 urls = ['http://mohp.gov.np/', 'http://nepalindata.com/resource/category/laws-policies-and-strategies/', 'http://nepalconsular.gov.np/np/',
-        'http://www.dotm.gov.np/', 'http://mofa.gov.np/', 'http://merolagani.com/', 'http://www.dop.gov.np/', 'http://ciaa.gov.np/','http://www.gatsby.ucl.ac.uk/teaching/courses/ml1-2016.html']
-i=0
+        'http://www.dotm.gov.np/', 'http://mofa.gov.np/', 'http://www.dop.gov.np/', 'http://ciaa.gov.np/','http://www.gatsby.ucl.ac.uk/teaching/courses/ml1-2016.html']
+# i=0
 
-# urls=['http://www.gatsby.ucl.ac.uk/teaching/courses/ml1-2016.html']
+# urls=['http://mohp.gov.np/']
+def date(filepath):
+    try:
+      pdf=fitz.open(filepath)
+      meta=pdf.metadata          
+      date=meta['creationDate']
+      if len(date)<14:                
+          f_date=date
+      else:
+          date=meta['creationDate'].split(':')[1]
+          f_date=date[0:4]+"-"+date[4:6]+"-"+date[6:8]+" "+date[8:10]+":"+date[10:12]+":"+date[12:14]
+      return f_date
+
+    except RuntimeError:
+      pass
+def language(texts):
+    special_characters = "!@#$%^&*()-+?_=,<>/[]{}|\~`"     
+    j=0
+    n_count=0
+    e_count=0
+   
+    text=texts.split(' ')
+    # print(text)
+    for txt in text:
+        txt=text[j]
+        if any(c in special_characters for c in txt) or  txt.isnumeric()==True :       
+            # print(txt) 
+            
+            j=j+1     
+            
+            continue
+        else:
+            if(len(txt)>=2 and len(txt)<=10):
+                # print(txt)
+                if (detect(txt)=='ne' or detect(txt)=='hi'):
+                    n_count=n_count+1
+                else:
+                    e_count=e_count+1   
+        j=j+1  
+    # print("Nepali:", n_count)
+    # print("English:" ,e_count) 
+    if(n_count>e_count):
+       return "Nepali"
+    else:
+       return "English" 
+
+
 
 for url in urls:
-    print(url)
-
+  
     drive='D:\\'
     folder='data'
  
@@ -39,28 +87,35 @@ for url in urls:
 
     # print(folder_location)
     if not os.path.exists(folder_location):os.mkdir(folder_location)
-
-    response = requests.get(url)
-    soup= BeautifulSoup(response.text, "html.parser")     
-
+    try:
+        response = requests.get(url)
+        soup= BeautifulSoup(response.text, "html.parser")
+        for title in soup.find_all('title'):
+         title_name=title.get_text()
+    except requests.exceptions.SSLError:
+        pass 
+    i=0
     for link in soup.select("a[href$='.pdf']"):
-    
-        #sees anchor tag , check hrefs,$ means end of the string and at end searches .pdf extension
-        #Name the pdf files using the last portion of each link which are unique in this case
+        a_title=link.parent.contents[0].text
+        print(a_title)
+     
+        
+        file_actual=os.path.join(folder_location,link['href'])
         filename = os.path.join(folder_location,link['href'].split('/')[-1])
-        print(filename)
-        # print(i)
-        # for file in list:
-        #     if(file==filename):
-        #         break
-        #     else:     
-        #         list=list+[filename]
-        with open(filename, 'wb') as f:
+        print(file_actual)
+        # try:
+        #  tag=soup.find_all("div", {"class": "text"})[i]
+        #  print(" ")
+        #  print(tag.find('a').contents)
+        # except AttributeError:
+        #     pass
+        # i=i+1  
+        try:
+         with open(filename, 'wb') as f:
             
             file_in_db=os.path.join(directory,filename)
             b_name=os.path.basename(filename)
-            file_in_db=os.path.join(directory,b_name)
-            
+            file_in_db=os.path.join(directory,b_name)          
            
             f.write(requests.get(urljoin(url,link['href'])).content)
             f.close()
@@ -69,26 +124,16 @@ for url in urls:
             #changes IN TITLE
             filepath=os.path.join(drive,folder,directory,b_name)
             # i=i+1
-            pdf=fitz.open(filepath)
-            meta=pdf.metadata          
-          
-
-            #CHANGES IN TIME
-            date=meta['creationDate']
-            
-            if len(date)<14:
-               
-                f_date=date
-            else:
-                date=meta['creationDate'].split(':')[1]
-                f_date=date[0:4]+"-"+date[4:6]+"-"+date[6:8]+" "+date[8:10]+":"+date[10:12]+":"+date[12:14]
-            print("date",f_date)
+            f_date=date(filepath)
 
             # language_OF_file=language(filepath)
-            language_OF_file='NOt recognized'
-            file2=files(title=file_in_db,creation_date=f_date,institution=directory,url=url,language=language_OF_file)        
+            language_OF_file=language(a_title)
+            print(language_OF_file)
+            file2=files(title=a_title,fullfile=file_in_db,creation_date=f_date,institution=directory,url=file_actual,language=language_OF_file)        
           
             file2.save()
-            
+     
+        except requests.exceptions.Timeout: 
+             pass
 
 
